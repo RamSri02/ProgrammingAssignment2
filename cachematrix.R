1b6c4c036eae1ab40660bac25289fb81b9a6fdeb
Sriram <sri1729.p@gmail.com>
2018-01-15 22:23:38 -0600

    Programming Assignment 2
    I have completed the assignment.

--- cachematrix.R
+++ cachematrix.R
@@ -3,8 +3,21 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix(), n) {
+    inverse <- NULL
+    set <- function(y)
+      {
+        x <<- y
+        inverse <<- NULL
+      }
+    get <- function()
+      {	
+        dim(x) <- c(n, n)
+        x
+      }
+    setInverse <- function(z) inverse <<- z
+    getInverse <- function() inverse
+    list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
 
@@ -12,4 +25,17 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    inverse <- x$getInverse()
+    if(!is.null(inverse))
+      {
+        message("getting cached data")
+        return(inverse)
+      }
+    Data <- x$get()
+    if(nrow(Data) == ncol(Data) && det(Data))
+      {
+        inverse <- solve(Data)
+        x$setInverse(inverse)
+        inverse
+      }
 }
